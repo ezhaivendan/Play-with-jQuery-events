73781c26467616084ac74d7f16b5ef379d185721
Muthukumar Chidambaram <ezhaivendan@gmail.com>
2022-08-04 22:38:04 +0530

    Adding Script File

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const data = {
+    "txt" : "hey",
+    "details" : [
+        {
+            "date" : "date1",
+            "name" : "name1"
+        },
+        {
+            "date" : "date2",
+            "name" : "name2"
+        },
+        {
+            "date" : "date3",
+            "name" : "name3"
+        }
+    ]
+}
+
+$(document).ready(function() {
+    $(".input-txt").focusout(function(){
+    let txtVal = $('.input-txt').val();
+    if(txtVal === data.txt) {
+        alert(`welcome ${txtVal}`);
+        if(data.details.length > 1) {
+            $('.bind-date').addClass('hidden');
+            $('.bind-multiple-dates').removeClass('hidden');
+            $('.bind-multiple-dates').append($("<option>Select Dates</option>"))
+            const appendOption = data.details.map( ele => {
+                $('.bind-multiple-dates').append($("<option></option>")
+                .attr("value", ele.date)
+                .text(ele.date));
+            })
+            $('.bind-multiple-dates').on('change', function() {
+                alert(this.value)
+                if(this.value === 'Select Dates'){
+                    alert("Please choose the proper date");
+                    $('.bind-name').val('')
+                }
+                let getDate = data.details.filter(ele => ele.date === this.value);
+                $('.bind-name').val(getDate[0].name)
+            });
+        } else if(data.details.length == 1) {
+            alert('hello')
+            $('.bind-date').val(data.details[0].date)
+            $('.bind-name').val(data.details[0].name)
+        }
+    }
+    else{
+        alert('Please check your input')
+        $('.bind-date').removeClass('hidden');
+        $('.bind-multiple-dates').addClass('hidden');
+        $('.bind-multiple-dates').find('option').remove()
+        $('.bind-name').val('')
+    }
+    })
+});
